fca402cbf7fd7dd50209ec3183b1964a0dc2b4b5
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-18 06:30:46 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,5 +21,9 @@ class InteractiveRecord
     column.names.compact
   end 
   
-  
+  def initialize(options={})
+    options.each do |property, value|
+      self.send ("#{property}=", value)
+    end 
+  end 
 end
